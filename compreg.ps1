54b0fca24fde980ef3df522086aafbc0765e0a50
Code-alaly <42795544+Code-alaly@users.noreply.github.com>
2019-10-15 15:54:26 -0700

    committed to learning github

--- compreg.ps1
+++ compreg.ps1
@@ -123,7 +123,7 @@ $info = "For $UCINetID in $dep; at $loc. Inputted by $me."
 
 #removes un-needed output by sending it to tmp file and deleting it
 
-Invoke-WebRequest 'http://apps.oit.uci.edu/mobileaccess/admin/mac/add_bulk.php' | Out-File "Recycle Bin"
+Invoke-WebRequest 'http://apps.oit.uci.edu/mobileaccess/admin/mac/add_bulk.php' | Out-File "Recycle Bin" -ErrorAction SilentlyContinue
 
 if (!(Get-Process -Name iexplore -ErrorAction SilentlyContinue)) {
     Start-Process 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList 'http://apps.oit.uci.edu/mobileaccess/admin/mac/add_bulk.php'
